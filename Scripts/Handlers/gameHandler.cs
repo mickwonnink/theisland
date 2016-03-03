f02e297468c1b14955717e5b2e33f65700242f1c
Mick Wonnink <m.wonnink@student.fontys.nl>
2016-03-03 19:28:50 +0100

    more work

--- Scripts/Handlers/gameHandler.cs
+++ Scripts/Handlers/gameHandler.cs
@@ -28,6 +28,8 @@ public class gameHandler : MonoBehaviour {
     float minViewSize = 3;
     float yStartPos = 215;
 
+    int[] possibleRot = new int[6] { 0, 60, 120, 180, 240, 300 };
+
     int amountOfPlayers = 4; //amount of player playing
     int currentPlayer = 1; //player number of player that needs to play his turn.
 
@@ -39,8 +41,9 @@ public class gameHandler : MonoBehaviour {
         mainViewCam = GameObject.Find("Camera");
 
         //island prefabs
-        GameObject woodland = (GameObject)Resources.Load("woodLand") as GameObject;
+        GameObject woodland = (GameObject)Resources.Load("forestLand") as GameObject;
         GameObject beachLand = (GameObject)Resources.Load("beachLand") as GameObject;
+        GameObject diamondLand = (GameObject)Resources.Load("diamondLand") as GameObject;
 
         int line1 = 3;
         int line2 = 9;
@@ -59,23 +62,23 @@ public class gameHandler : MonoBehaviour {
             {
                 if (i < line1)
                 {
-                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance * i), 1000, yStartPos), Quaternion.identity);
+                    islandPart = (GameObject)Instantiate(diamondLand, new Vector3(-(horizontalDistance * i), 1000, yStartPos), Quaternion.Euler(new Vector3(0, possibleRot[Random.Range(0, possibleRot.Length)], 0)));
                 }
                 else if (i < line2)
                 {
-                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance * 0.5f) - (horizontalDistance * i) + (line1 * horizontalDistance), 1000, yStartPos + verticleDistance), Quaternion.identity);
+                    islandPart = (GameObject)Instantiate(diamondLand, new Vector3(-(horizontalDistance * 0.5f) - (horizontalDistance * i) + (line1 * horizontalDistance), 1000, yStartPos + verticleDistance), Quaternion.Euler(new Vector3(0, possibleRot[Random.Range(0, possibleRot.Length)], 0)));
                 }
                 else if (i < line3)
                 {
-                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance * i) + (line2 * horizontalDistance), 1000, yStartPos + (verticleDistance * 2)), Quaternion.identity);
+                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance * i) + (line2 * horizontalDistance), 1000, yStartPos + (verticleDistance * 2)), Quaternion.Euler(new Vector3(0, possibleRot[Random.Range(0, possibleRot.Length)], 0)));
                 }
                 else if (i < line4)
                 {
-                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance * 1.5f) - (horizontalDistance * i) + (line3 * horizontalDistance), 1000, yStartPos + (verticleDistance * 3)), Quaternion.identity);
+                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance * 1.5f) - (horizontalDistance * i) + (line3 * horizontalDistance), 1000, yStartPos + (verticleDistance * 3)), Quaternion.Euler(new Vector3(0, possibleRot[Random.Range(0, possibleRot.Length)], 0)));
                 }
                 else
                 {
-                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance) - (horizontalDistance * i) + (line4 * horizontalDistance), 1000, yStartPos + (verticleDistance * 4)), Quaternion.identity);
+                    islandPart = (GameObject)Instantiate(woodland, new Vector3(-(horizontalDistance) - (horizontalDistance * i) + (line4 * horizontalDistance), 1000, yStartPos + (verticleDistance * 4)), Quaternion.Euler(new Vector3(0, possibleRot[Random.Range(0, possibleRot.Length)], 0)));
                 }
 
             }
@@ -87,7 +90,7 @@ public class gameHandler : MonoBehaviour {
     float zoomFactor = 0;
     Vector3 lastPos = new Vector3();
     float mouseSensetivity = 1;
-
+    GameObject lastHover = null;
 	
 	// Update is called once per frame
 	void Update () {
@@ -134,6 +137,33 @@ public class gameHandler : MonoBehaviour {
         {
             lastPos = new Vector3();
         }
+
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //draw invisible ray cast/vector
+               // Debug.DrawLine(ray.origin, hit.point);
+                //log hit area to the console
+                Debug.Log("hitpoint: " + hit.point);
+            if (hit.collider.transform.tag == "island")
+            {
+                GameObject child = hit.collider.transform.parent.FindChild("Cylinder").gameObject;
+                if (child != null)
+                {
+                    child.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
+                    lastHover = child;
+                }
+            }
+
+            }
+
+
+            //Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Vector3 point = ray.origin + (ray.direction * 50);
+            //Debug.Log(point);
+        
 
     }
 }
---
+++ Scripts/TerrainInitialiser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainInitialiser : MonoBehaviour {
+
+    Color myOriginColor = new Color();
+
+	// Use this for initialization
+	void Start () {
+        myOriginColor = gameObject.GetComponent<Renderer>().material.color;
+	}
+
+    public void OnCollisionExit(Collision col)
+    {
+        gameObject.GetComponent<Renderer>().material.color = myOriginColor;
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("ONCOLLISIONENTER");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
